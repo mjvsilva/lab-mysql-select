3e0ceec3dfd4b76105d230e882d49e46b98b7cea
mjvsilva <56732470+mjvsilva@users.noreply.github.com>
2019-10-31 23:30:37 +0000

    update challenge 4 with COALESCE/LEFT JOIN

--- your-code/solution.sql
+++ your-code/solution.sql
@@ -43,10 +43,10 @@ SELECT a.au_id AS 'AUTHOR ID', a.au_lname AS 'LAST NAME', a.au_fname AS 'FIRST N
 
 
 # CHALLENGE 4: 23 best selling authors ranking
-SELECT a.au_id AS 'AUTHOR ID', a.au_lname AS 'LAST NAME', a.au_fname AS 'FIRST NAME', SUM(s.qty) AS 'TOTAL'
+SELECT a.au_id AS 'AUTHOR ID', a.au_lname AS 'LAST NAME', a.au_fname AS 'FIRST NAME', COALESCE(SUM(s.qty)) AS 'TOTAL'
 	FROM authors a
-	JOIN titleauthor ta ON ta.au_id=a.au_id
-	JOIN titles t ON ta.title_id=t.title_id
+	LEFT JOIN titleauthor ta ON ta.au_id=a.au_id
+	LEFT JOIN titles t ON ta.title_id=t.title_id
 	LEFT JOIN sales s ON s.title_id=t.title_id
 	GROUP BY a.au_id
 	ORDER BY SUM(s.qty) DESC
